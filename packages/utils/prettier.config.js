e94aa1bcb0d9f478dc11e0e1fe7b7419c1a8d1de
hhui64 <907322015@qq.com>
2025-03-31 02:07:31 +0800

    feat: 添加 ESLint 和 Prettier 配置文件，优化代码格式化和检查

---
+++ packages/utils/prettier.config.js
@@ -0,0 +1,12 @@
+import rootConfig from '../../prettier.config.js'
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import('prettier').Config}
+ */
+const config = {
+  printWidth: 80,
+  singleAttributePerLine: true
+}
+
+export default Object.assign(rootConfig, config)
